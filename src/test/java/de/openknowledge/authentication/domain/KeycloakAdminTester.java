25018f10b2e16c465963fbf56f07987ce0917cf5
Tim de Buhr <tim.debuhr@openknowledge.de>
2020-06-19 15:27:23 +0200

    chore(keycloak-admin-client-adapter): optimize test client

--- src/test/java/de/openknowledge/authentication/domain/KeycloakAdminTester.java
+++ src/test/java/de/openknowledge/authentication/domain/KeycloakAdminTester.java
@@ -31,115 +31,133 @@ public class KeycloakAdminTester {
   private static final GroupName PROGRAM_ANALYST = GroupName.fromValue("PROGRAM_ANALYST");
   private static final RoleName INTERACTION_MAINTAIN = RoleName.fromValue("INTERACTION_MAINTAIN");
 
+  private static final Username USERNAME = Username.fromValue("test.user42");
+  private static final Password PASSWORD = Password.fromValue("Test1234");
+  private static final Password RESET_PASSWORD = Password.fromValue("Test5678");
+  private static final EmailAddress EMAIL_ADDRESS = EmailAddress.fromValue("test.user42@domain.tld");
+
+  private static final Issuer ISSUER = Issuer.fromValue("keycloakAdmin");
+
+  private static final KeycloakServiceConfiguration SERVICE_CONFIG =
+      new KeycloakServiceConfiguration("harbor", "react-loyalty");
+  private static final KeycloakAdapterConfiguration ADAPTER_CONFIG =
+      new KeycloakAdapterConfiguration("http://localhost:8000/auth",
+      "master",
+      "admin-cli",
+      "admin",
+      "keycloak",
+      "password",
+      "5");;
+
+  private static final KeycloakAdapter ADAPTER = new KeycloakAdapter(ADAPTER_CONFIG);
+  private static final KeycloakUserService USER_SERVICE = createUserService();
+  private static final KeycloakLoginService LOGIN_SERVICE = createLoginService();
+  private static final KeycloakRegistrationService REGISTRATION_SERVICE = createRegisterService();
+
+  private static final UserAccount ACCOUNT = createAccount();
+
+
   public static void main(String[] args) throws URISyntaxException {
-    Username username = Username.fromValue("test.user42");
-    EmailAddress emailAddress = EmailAddress.fromValue("test.user42@domain.tld");
-    Password password = Password.fromValue("Test1234");
+    try {
+      USER_SERVICE.getUser(UserIdentifier.fromValue("47110815"));
+    } catch (UserNotFoundException e) {
+      System.out.println("user (id=47110815) not found as inspected");
+    }
 
-    KeycloakAdapterConfiguration adapterConfig = createAdapterConfig();
-    KeycloakKeyConfiguration keyConfig = createKeyConfig();
-    KeycloakServiceConfiguration serviceConfig = createServiceConfig();
-    KeycloakRegistrationServiceConfiguration registrationServiceConfiguration = createRegistrationServiceConfig();
-    Issuer issuer = Issuer.fromValue("keycloakAdmin");
+    UserAccount createdUserAccount = REGISTRATION_SERVICE.register(ACCOUNT);
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") created\n" + createdUserAccount);
 
-    KeycloakAdapter adapter = new KeycloakAdapter(adapterConfig);
-    KeycloakUserService userService = new KeycloakUserService(adapter, serviceConfig);
-    KeycloakTokenService tokenService = new KeycloakTokenService(keyConfig);
-    tokenService.init();
+    VerificationLink link = REGISTRATION_SERVICE.createVerificationLink(createdUserAccount, ISSUER);
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") get link " + link);
 
-    KeycloakRegistrationService registrationService = new KeycloakRegistrationService(serviceConfig,
-        registrationServiceConfiguration,
-        userService,
-        tokenService);
-    registrationService.init();
+    UserIdentifier identifier = REGISTRATION_SERVICE.verifyEmailAddress(link, ISSUER);
+    System.out.println("user (id=" + identifier + ") mail verified");
 
-    UserAccount userAccount = new UserAccount(username, emailAddress, null);
+    GroupName[] groupNames = new GroupName[] {END_CUSTOMER, PROGRAM_ANALYST};
+    USER_SERVICE.joinGroups(createdUserAccount.getIdentifier(), groupNames);
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") joins groups " + Arrays.toString(groupNames));
+
+    UserAccount foundUserAccount = USER_SERVICE.getUser(createdUserAccount.getIdentifier());
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") found as inspected\n" + foundUserAccount);
+
+    if (!foundUserAccount.getUsername().getValue().equals(USERNAME.getValue())) {
+      System.err.println("user (id=" + createdUserAccount.getIdentifier() + ") is not valid");
+    }
+
+    if (!foundUserAccount.getEmailAddress().getValue().equals(EMAIL_ADDRESS.getValue())) {
+      System.err.println("user (id=" + createdUserAccount.getIdentifier() + ") is not valid");
+    }
+
+    USER_SERVICE.leaveGroups(createdUserAccount.getIdentifier(), PROGRAM_ANALYST);
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") leaves groups " + PROGRAM_ANALYST);
+
+    USER_SERVICE.joinRoles(createdUserAccount.getIdentifier(), RoleType.CLIENT, INTERACTION_MAINTAIN);
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") joins client roles " + INTERACTION_MAINTAIN);
+
+    LoginToken loginToken = LOGIN_SERVICE.login(new Login(USERNAME, PASSWORD));
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") login token " + loginToken);
+
+    LoginToken refreshedLoginToken = LOGIN_SERVICE.refresh(RefreshToken.fromValue(loginToken.getRefreshToken()));
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") refreshed login token " + refreshedLoginToken);
+
+    LOGIN_SERVICE.logout(createdUserAccount.getIdentifier());
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") logged out");
+
+    USER_SERVICE.resetPassword(createdUserAccount.getIdentifier(), RESET_PASSWORD);
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") reset password");
+
+    LoginToken loginTokenAfterReset = LOGIN_SERVICE.login(new Login(USERNAME, RESET_PASSWORD));
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") login token " + loginTokenAfterReset);
+
+    LOGIN_SERVICE.logout(createdUserAccount.getIdentifier());
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") logged out");
+
+    USER_SERVICE.deleteUser(createdUserAccount.getIdentifier());
+    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") deleted");
+  }
+
+  private static UserAccount createAccount() {
+    UserAccount userAccount = new UserAccount(USERNAME, EMAIL_ADDRESS, PASSWORD);
     userAccount.setName(Name.fromValues(FirstName.fromValue("test"), LastName.fromValue("user")));
     userAccount.addAttribute(new Attribute("userAttributeKey", "userAttributeValue"));
     userAccount.addAttribute(new Attribute("userAttributeListKey", "userAttributeListValueOne"));
     userAccount.addAttribute(new Attribute("userAttributeListKey", "userAttributeListValueTwo"));
     userAccount.addAttribute(new Attribute("userAttributeListKey", "userAttributeListValueThree"));
-
-    try {
-      userService.getUser(UserIdentifier.fromValue("47110815"));
-    } catch (UserNotFoundException e) {
-      System.out.println("user (id=47110815) not found as inspected");
-    }
-
-    UserAccount createdUserAccount = registrationService.register(userAccount);
-    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") created\n" + createdUserAccount);
-
-    VerificationLink link = registrationService.createVerificationLink(createdUserAccount, issuer);
-    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") get link " + link);
-
-    UserIdentifier identifier = registrationService.verifyEmailAddress(link, issuer);
-    System.out.println("user (id=" + identifier + ") mail verified");
-
-    GroupName[] groupNames = new GroupName[] { END_CUSTOMER, PROGRAM_ANALYST };
-    userService.joinGroups(createdUserAccount.getIdentifier(), groupNames);
-    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") joins groups " + Arrays.toString(groupNames));
-
-    UserAccount foundUserAccount = userService.getUser(createdUserAccount.getIdentifier());
-    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") found as inspected\n" + foundUserAccount);
-
-    if (!foundUserAccount.getUsername().getValue().equals(username.getValue())) {
-      System.err.println("user (id=" + createdUserAccount.getIdentifier() + ") is not valid");
-    }
-
-    if (!foundUserAccount.getEmailAddress().getValue().equals(emailAddress.getValue())) {
-      System.err.println("user (id=" + createdUserAccount.getIdentifier() + ") is not valid");
-    }
-
-    userService.leaveGroups(createdUserAccount.getIdentifier(), PROGRAM_ANALYST);
-    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") leaves groups " + PROGRAM_ANALYST);
-
-    userService.resetPassword(createdUserAccount.getIdentifier(), password);
-    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") reset password");
-
-    userService.joinRoles(createdUserAccount.getIdentifier(), RoleType.CLIENT, INTERACTION_MAINTAIN);
-    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") joins client roles " + INTERACTION_MAINTAIN);
-
-    KeycloakLoginService loginService = new KeycloakLoginService(adapter, serviceConfig);
-    loginService.init();
-    LoginToken loginToken = loginService.login(new Login(username, password));
-    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") login token " + loginToken);
-
-    LoginToken refreshedLoginToken = loginService.refresh(RefreshToken.fromValue(loginToken.getRefreshToken()));
-    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") refreshed login token " + refreshedLoginToken);
-
-    loginService.logout(createdUserAccount.getIdentifier());
-    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") logged out");
-
-    userService.deleteUser(createdUserAccount.getIdentifier());
-    System.out.println("user (id=" + createdUserAccount.getIdentifier() + ") deleted");
+    return userAccount;
   }
 
-  private static KeycloakAdapterConfiguration createAdapterConfig() {
-    return new KeycloakAdapterConfiguration("http://localhost:8000/auth",
-        "master",
-        "admin-cli",
-        "admin",
-        "keycloak",
-        "password",
-        "5");
-  }
-
-  private static KeycloakKeyConfiguration createKeyConfig() {
-    return new KeycloakKeyConfiguration("public.key",
+  private static KeycloakRegistrationService createRegisterService() {
+    KeycloakKeyConfiguration keyConfig = new KeycloakKeyConfiguration("public.key",
         "private.key",
         "bb401f71e743458186e90541603fcace",
         "RSA");
-  }
 
-  private static KeycloakServiceConfiguration createServiceConfig() {
-    return new KeycloakServiceConfiguration("harbor",
-        "react-loyalty");
-  }
+    KeycloakTokenService tokenService = new KeycloakTokenService(keyConfig);
+    tokenService.init();
 
-  private static KeycloakRegistrationServiceConfiguration createRegistrationServiceConfig() {
-    return new KeycloakRegistrationServiceConfiguration("DOUBLE_OPT_IN",
+    KeycloakRegistrationServiceConfiguration registrationServiceConfig = new KeycloakRegistrationServiceConfiguration("DOUBLE_OPT_IN",
         "ROLE",
         "5",
         "MINUTES");
+
+    KeycloakRegistrationService registrationService = new KeycloakRegistrationService(SERVICE_CONFIG,
+        registrationServiceConfig,
+        USER_SERVICE,
+        tokenService);
+    registrationService.init();
+    return registrationService;
   }
+
+  private static KeycloakUserService createUserService() {
+    KeycloakUserService userService = new KeycloakUserService(ADAPTER, SERVICE_CONFIG);
+    userService.init();
+    return userService;
+  }
+
+  private static KeycloakLoginService createLoginService() {
+    KeycloakLoginService loginService = new KeycloakLoginService(ADAPTER, SERVICE_CONFIG);
+    loginService.init();
+    return loginService;
+  }
+
 }
